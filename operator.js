33443b2b92d11b0c6866f7458dd7d501d893c2d1
akshaypree22 <akshaypree22@gmail.com>
2025-01-21 08:51:48 +0530

    classcompEG

---
+++ operator.js
@@ -0,0 +1,25 @@
+var a = 5
+var b = '5'
+console.log(a==b)
+console.log(a == b)
+var c = 10
+console.log(-c)
+for (let [i,j] of Object.values(b)){
+    consol     
+
+function database ()P{
+     dbname = "SECE"
+    return [dbname,"Data stored successfully"]
+}
+    onsole.log("inside timeout")
+},50000
+ console.log(output);
+}
+asaw().then (function(output)){
+    console.log(output
+        
+    )
+}
+
+}
+)
---
+++ sample_project/src/assets/classcompEG.jsx
@@ -0,0 +1,10 @@
+import react from "react"; 
+class ClassCompEG extends Redact.Component{
+    return(){
+        <div>
+        <h3>This is a class Component ERxample.</h3>
+        </div>
+
+}
+}
+export default classComponent
